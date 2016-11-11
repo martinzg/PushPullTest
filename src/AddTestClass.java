6c390f3c3837796dd7573513c88af4c773439979
martinzg <martinzg@inbox.lv>
2016-11-11 14:05:03 +0200

    Update AddTestClass.java

--- src/AddTestClass.java
+++ src/AddTestClass.java
@@ -1,8 +1,7 @@
-public class AddTestClass {
-
-    int a = 0;
-    int b = 1;
-    int c = 2;
-    int d = 3;
-
+@Test
+public void should_be_able_to_login_with_credentials() {
+  givenThat(anna).wasAbleTo(openTheMeetUpWebsite);
+  when(anna).wasAbleTo(browseToTheLoginPage);
+    and(anna).attemptsTo(LogIn.withCredentials());
+  then(anna).should(eventually(seeThat(theUserAvatarIsVisible)));
 }
