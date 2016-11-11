2018e4a2438ee3a0796848b7ed7c03a148371286
martinzg <martinzg@inbox.lv>
2016-11-11 15:27:51 +0200

    Update AddTestClass.java

--- src/AddTestClass.java
+++ src/AddTestClass.java
@@ -1,7 +1,2 @@
-@Test
-public void should_be_able_to_login_with_credentials() {
-  givenThat(anna).wasAbleTo(openTheMeetUpWebsite);
-  when(anna).wasAbleTo(browseToTheLoginPage);
-    and(anna).attemptsTo(LogIn.withCredentials());
-  then(anna).should(eventually(seeThat(theUserAvatarIsVisible)));
-}
+tim.can(Authenticate.withCredentials("username","password"));
+tim.attemptsTo(Login.withCredentials());
