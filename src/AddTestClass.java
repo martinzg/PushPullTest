7990cbdbd2473282920634121bb4abe65a647509
martinzg <martinzg@inbox.lv>
2016-11-11 15:57:59 +0200

    Update AddTestClass.java

--- src/AddTestClass.java
+++ src/AddTestClass.java
@@ -1,3 +1,6 @@
-givenThat(james).wasAbleTo(openTheWebshop);
-when(james).attemptsTo(AddsToTheCart.theProducts("Product"));
-then(james)...;
+public class MyTask implements Task {
+  @Override
+  public <T extends Actor> void performAs(T actor) {
+    // Here the actor performs the actions to complete the task
+  }
+}
